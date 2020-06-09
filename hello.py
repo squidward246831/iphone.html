586843bef04181b3d0b0fed6e74a09911a75b1d6
Your Name <you@example.com>
2020-06-08 20:55:25 -0500

    just fixed some stuff

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print("hello world")
+from Flask import Flask
+app = Flask(_name_)
+@app.route("/")
+def index():
+    return("Hello World!!")
+@app.route("/<string:name")
+def hello(name):
+    return("Hello, {name}!")    
