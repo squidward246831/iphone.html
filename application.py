35d90151accdba358e1b0beb92ec0af75f19e318
Your Name <you@example.com>
2020-06-11 13:19:59 -0500

    added some stuff

--- application.py
+++ application.py
@@ -5,9 +5,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return("Hello World!!")
-
+    return("<h1>Hello World!!</h1>")
+    return("<em>I hope everyone is having a wonderful day</em>")
+    return("Today is quite nice")
 
 @app.route("/<string:name>")
 def hello(name):
-    return f"Hello, {name}!"
+    return f"<h1>Hello, {name}!</em>"
+    return f"<em>I hope you are having a wonderful day, {name} </em>"
+    return f"<h2>The weather is quite nice today right, {name}</h2>"
+    
